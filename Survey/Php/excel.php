ca1e588488189abf57ec6c3b9ba791d3a9c16205
Alfredo Reyes Montero <reyes-fred@hotmail.com>
2018-01-02 18:15:29 -0600

    Create excel.php
    Update

---
+++ Survey/Php/excel.php
@@ -0,0 +1,33 @@
+<?php
+
+$escuela = $_GET['nombre'];
+$a1 = $_GET['a1'];
+$a2 = $_GET['a2'];
+$a3 = $_GET['a3'];
+$a4 = $_GET['a4'];
+
+
+            
+$name = 'info.csv';
+if(file_exists($name)){
+    $file = fopen('info.csv', 'a');  
+}
+else{
+    $file = fopen('info.csv', 'w') or die("Unable to open file!");
+    //set the column names
+    $cells[] = array('Visita','Fecha','Pregunta 1', 'Pregunta 2', 'Pregunta 3', 'Pregunta 4');
+}
+
+ 
+//pass all the form values
+$cells[] = array($escuela,date("m/d/y"),$a1, $a2, $a3, $a4);
+ 
+foreach($cells as $cell){
+	fputcsv($file,$cell);
+}
+ 
+fclose($file); 
+
+header("Location:../index.html");
+exit;
+?>
