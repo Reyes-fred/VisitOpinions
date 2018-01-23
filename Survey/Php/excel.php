9ed3497477dc0b4811a17119500f2959c2462c7c
Alfredo Reyes <reyes-fred@hotmail.com>
2018-01-23 15:54:00 -0800

    Update

--- Survey/Php/excel.php
+++ Survey/Php/excel.php
@@ -1,10 +1,12 @@
 <?php
 
 $escuela = $_GET['nombre'];
+$calif = $_GET['calif'];
 $a1 = $_GET['a1'];
 $a2 = $_GET['a2'];
 $a3 = $_GET['a3'];
 $a4 = $_GET['a4'];
+$a4 = $_GET['a5'];
 
 
             
@@ -15,12 +17,11 @@ if(file_exists($name)){
 else{
     $file = fopen('info.csv', 'w') or die("Unable to open file!");
     //set the column names
-    $cells[] = array('Visita','Fecha','Pregunta 1', 'Pregunta 2', 'Pregunta 3', 'Pregunta 4');
+    $cells[] = array('Visita','Fecha','Pregunta 1', 'Pregunta 2', 'Pregunta 3', 'Pregunta 4','Pregunta 5','Pregunta 6');
 }
-
  
 //pass all the form values
-$cells[] = array($escuela,date("m/d/y"),$a1, $a2, $a3, $a4);
+$cells[] = array($escuela,date("m/d/y"),$a1, $a2, $a3, $a4, $a5, $calif);
  
 foreach($cells as $cell){
 	fputcsv($file,$cell);
